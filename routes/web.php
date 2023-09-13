fd26f1935e65bd435b559228ccd23c2654fb1f3b
Churchill Capagngan <cbcapagngan@student.apc.edu.ph>
2023-09-13 16:42:22 +0800

    modify routing for user depends on role

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,42 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Auth::routes();
+
+//Student
+Route::middleware(['auth', 'role:Student'])->group(function () {
+    Route::get('/student/home', [HomeController::class, 'studentHome'])->name('studentHome');
+});
+
+//Faculty
+Route::middleware(['auth', 'role:Faculty'])->group(function () {
+    Route::get('/faculty/home', [HomeController::class, 'facultyHome'])->name('facultyHome');
+});
+
+//Staff
+Route::middleware(['auth', 'role:Staff'])->group(function () {
+    Route::get('/staff/home', [HomeController::class, 'staffHome'])->name('staffHome');
+});
+
+//Clinic
+Route::middleware(['auth', 'role:Clinic'])->group(function () {
+    Route::get('/clinic/home', [HomeController::class, 'clinicHome'])->name('clinicHome');
+});
+
+//Doctor
+Route::middleware(['auth', 'role:Doctor'])->group(function () {
+    Route::get('/doctor/home', [HomeController::class, 'doctorHome'])->name('doctorHome');
+});
+
+//Dentist
+Route::middleware(['auth', 'role:Dentist'])->group(function () {
+    Route::get('/dentist/home', [HomeController::class, 'dentistHome'])->name('dentistHome');
+});
+
+//Admin
+Route::middleware(['auth', 'role:Admin'])->group(function () {
+    Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('adminHome');
 });
