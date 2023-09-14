ed338ccc62a87e6a2e2d2a4c03e88341b3a76832
Churchill Capagngan <cbcapagngan@student.apc.edu.ph>
2023-09-14 21:00:50 +0800

    added side menu access depends on role

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,55 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Gate::define('student', function($user){
+            if(auth()->user()->role->role == 'Student'){
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('faculty', function($user){
+            if(auth()->user()->role->role == 'Faculty'){
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('staff', function($user){
+            if(auth()->user()->role->role == 'Staff'){
+                return true;
+            }
+            return false;
+        });
+        
+        Gate::define('nurse', function($user){
+            if(auth()->user()->role->role == 'Nurse'){
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('doctor', function($user){
+            if(auth()->user()->role->role == 'Doctor'){
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('dentist', function($user){
+            if(auth()->user()->role->role == 'Dentist'){
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('admin', function($user){
+            if(auth()->user()->role->role == 'Admin'){
+                return true;
+            }
+            return false;
+        });
     }
 }
