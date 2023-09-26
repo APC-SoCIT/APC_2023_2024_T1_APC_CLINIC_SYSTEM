6e2bb8d799a387c45ca0ed558d42e3267186b052
Churchill Capagngan <cbcapagngan@student.apc.edu.ph>
2023-09-27 01:50:38 +0800

    arrange role_id

--- database/migrations/2023_09_13_064743_create_users_table.php
+++ database/migrations/2023_09_13_064743_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('role_id')->nullable();  // Add role_id
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->unsignedBigInteger('role_id')->nullable();  // Add role_id
             $table->timestamps();
 
             // Add foreign key constraint 
