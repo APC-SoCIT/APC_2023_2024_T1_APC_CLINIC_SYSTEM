9a8d246dee7c764d3dc9be99f822a8652b2ea995
Churchill Capagngan <cbcapagngan@student.apc.edu.ph>
2023-09-29 10:44:16 +0800

    added new table that are fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'school_id',
+        'course',
+        'specialization',
+        'grade',
+        'year',
+        'section',
     ];
 
     /**
